6d6496d27ddef5a510ddc38012fef3fad8985cb5
Vicente Garza Reyna <vicente.garzara@uanl.edu.mx>
2021-05-13 03:19:38 -0500

    Added log message on start

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	config "northwindApi/config"
 	routing "northwindApi/routing"
@@ -22,6 +23,7 @@ func main() {
 	router := mux.NewRouter()
 
 	routing.Router(router)
+	log.Println("starting")
 
 	//CORS
 	err := http.ListenAndServe(":5000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Request-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(router))
